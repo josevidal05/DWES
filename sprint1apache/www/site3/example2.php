cbc5cb13d32367c8b87d2ac5094cd13c492f3f51
josevidal05 <josevidalfernandez5@gmail.com>
2024-10-16 13:59:43 +0200

    Ej 2: Intro php

---
+++ sprint1apache/www/site3/example2.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<h1>Página de bienvenidas</h1>
+<?php
+function dar_bienvenida($nombre) {
+echo "¡Bienvenido, " . $nombre . "!";
+}
+dar_bienvenida("Hommer Simpson");
+?>
+</body>
+</html>
