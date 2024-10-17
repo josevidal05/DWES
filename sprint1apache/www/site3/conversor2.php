cb565331f4cf64694d9783ea8976a4797a0c30eb
josevidal05 <josevidalfernandez5@gmail.com>
2024-10-17 11:55:25 +0200

    Ej 6: Intro php

--- sprint1apache/www/site3/conversor2.php
+++ sprint1apache/www/site3/conversor2.php
@@ -2,13 +2,20 @@
 <body>
 <h1>Conversor de longitudes</h1>
 <p>Convierte de la unidad especificada a metros</p>
-<p>
+<p176g>
 <?php
 if (isset($_POST["funidad"])) {
 if ($_POST["funidad"] == "pulgada") {
 $v_pulgadas = $_POST["fcantidad"];
 $v_metros = $v_pulgadas * 0.0254;
 echo $v_pulgadas."pulgada(s) = ".$v_metros." metro(s)";
+}
+
+elseif ($_POST["funidad"] == "pie") {
+$v_pulgadas = $_POST["fcantidad"];
+$v_metros = $v_pulgadas * 0.3048;
+
+echo $v_pulgadas."pie(s) = ".$v_metros." metro(s)";
 } else {
 echo "Unidad no soportada";
 }
@@ -17,12 +24,19 @@ echo "Unidad no soportada";
 </p>
 <p>Realiza una nueva conversión:</p>
 <form action="/conversor2.php" method="post">
+
 <label for="cantidad_input">Cantidad:</label><br>
 <input type="text" id="cantidad_input" name="fcantidad"><br>
+
 <input type="radio" id="pulgada_input" name="funidad" value="pulgada">
 <label for="pulgada_input">Pulgada(s)</label><br>
+
+<input type="radio" id="pie_input" name="funidad" value="pie">
+<label for="pie_input">Pies(s)</label><br>
+
 <input type="radio" id="otro_input" name="funidad" value="otro">
 <label for="otro_input">Otro</label><br>
+
 <input type="submit" value="Convertir">
 </form>
 </body>
