f0c234d487eee9bd5f021a93fda5b37fae80690a
josevidal05 <josevidalfernandez5@gmail.com>
2024-11-13 14:23:10 +0100

    3.2: Ej 1

---
+++ sprint3python/operaciones.py
@@ -0,0 +1,15 @@
+
+
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+def multiplicacion(a, b):
+    return a * b
+
+def division(a, b):
+    if b == 0:
+        return "Error: División por 0 no permitida"
+    return a / b
