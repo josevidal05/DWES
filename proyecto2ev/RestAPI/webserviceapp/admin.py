47dd115412eb1b789c3bb588b1edd0a32dcba8ab
José Vidal <josevidalfernandez5@gmail.com>
2025-01-20 23:10:02 +0100

    Administrador

--- proyecto2ev/RestAPI/webserviceapp/admin.py
+++ proyecto2ev/RestAPI/webserviceapp/admin.py
@@ -5,7 +5,23 @@ from django.contrib import admin
 from django.contrib import admin
 from .models import User, Evento, Reserva, Comentario
 
-admin.site.register(User)
-admin.site.register(Evento)
-admin.site.register(Reserva)
-admin.site.register(Comentario)
+@admin.register(User)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'email', 'tipo', 'biografia')
+    list_filter = ('tipo',)
+
+@admin.register(Evento)
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'fecha', 'limiteMaximo', 'organizador')
+    search_fields = ('titulo', 'descripcion')
+    list_filter = ('fecha', 'organizador')
+
+@admin.register(Reserva)
+class ReservaAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'evento', 'entradas')
+    list_filter = ('estado',)
+
+@admin.register(Comentario)
+class ComentarioAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'infoEvento', 'fecha')
+    search_fields = ('texto',)
